67ba7b4455e03683d5bef40102a1bfb1c78df6d7
Jordan Hudgens <jordan@devcamp.com>
2017-07-05 15:45:16 +0000

    Added nav bar and added default header content

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -5,12 +5,15 @@ class BlogsController < ApplicationController
   # GET /blogs.json
   def index
     @blogs = Blog.all
+    @page_title = "My Portfolio Blog"
   end
 
   # GET /blogs/1
   # GET /blogs/1.json
   def show
     @blog = Blog.find(3)
+    @page_title = @blog.title
+    @seo_keywords = @blog.body
   end
 
   # GET /blogs/new
