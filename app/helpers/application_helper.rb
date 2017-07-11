913091492ba9d3305a55b35c2ee2a39751c7197d
Jordan Hudgens <jordan@devcamp.com>
2017-07-11 14:49:59 +0000

    Integrated the gem into rails app for copyright

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -15,4 +15,8 @@ module ApplicationHelper
       content_tag(:p, greeting, class: "source-greeting")
     end
   end
+  
+  def copyright_generator
+    @copyright = ArmadilloViewTool::Renderer.copyright 'Rachel Parker', 'All rights reserved'
+  end
 end
